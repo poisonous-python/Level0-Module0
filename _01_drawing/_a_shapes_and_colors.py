3234c080592b1f5f02a34fe7a32a99e17ff99575
Eric Busboom <eric@civicknowledge.com>
2024-02-11 00:46:38 -0800

    Update _a_shapes_and_colors.py

--- _01_drawing/_a_shapes_and_colors.py
+++ _01_drawing/_a_shapes_and_colors.py
@@ -1,36 +1,36 @@
 import turtle
 
-if __name__ == '__main__':
-    window = turtle.Screen()
-    window.bgcolor('white')
 
-    # This code makes a new Turtle. Pick a new name for the turtle
-    my_turtle = turtle.Turtle()
+window = turtle.Screen()
+window.bgcolor('white')
 
-    # Make your turtle's shape 'turtle', .shape('turtle')
+# This code makes a new Turtle. Pick a new name for the turtle
+my_turtle = turtle.Turtle()
 
-    # Set your turtle's speed using .speed(2)
+# Make your turtle's shape 'turtle', .shape('turtle')
 
-    # Set your turtle's color using .color('green') and .pencolor('blue')
+# Set your turtle's speed using .speed(2)
 
-    # Move your turtle forward using .forward(100)
-    # TEST    Did your turtle move forward?
+# Set your turtle's color using .color('green') and .pencolor('blue')
 
-    # Move your turtle left or right using .left(90) or .right(90)
+# Move your turtle forward using .forward(100)
+# TEST    Did your turtle move forward?
 
-    # Now put the forward and left/right code into a for loop to repeat 4 times.
-    # TEST    Did your turtle draw a square?
+# Move your turtle left or right using .left(90) or .right(90)
 
-    # Move your turtle to a new place on the screen using .goto(x, y)
-    # x=0 and y=0 is the center of the screen
+# Now put the forward and left/right code into a for loop to repeat 4 times.
+# TEST    Did your turtle draw a square?
 
-    # Have your turtle draw a circle using .circle(radius, steps=30)
-    # TEST    Did your turtle draw a circle?
+# Move your turtle to a new place on the screen using .goto(x, y)
+# x=0 and y=0 is the center of the screen
 
-    # Add color to your shape by adding .begin_fill() before drawing the circle
-    # and .end_fill() below
+# Have your turtle draw a circle using .circle(radius, steps=30)
+# TEST    Did your turtle draw a circle?
 
-    # Draw 3 more shapes with different fill colors!
+# Add color to your shape by adding .begin_fill() before drawing the circle
+# and .end_fill() below
 
-    # ===================== DO NOT EDIT THE CODE BELOW ============================
-    turtle.done()
+# Draw 3 more shapes with different fill colors!
+
+# ===================== DO NOT EDIT THE CODE BELOW ============================
+turtle.done()
